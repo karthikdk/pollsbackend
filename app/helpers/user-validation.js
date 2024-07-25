c79f76178e7ea4ecd02385153e379ea5783d8b5c
KARTHIK D <dk.karthikd9@gmail.com>
2024-07-25 10:57:16 +0530

    split validation to register and login

--- app/helpers/user-validation.js
+++ app/helpers/user-validation.js
@@ -1,27 +1,59 @@
-const userValidationSchema={
-    username:{
-        notEmpty:{
-            errorMessage:'username is required'
-        },
+const User = require("../models/user")
+
+
+const usernameSchema={
+    notEmpty:{
+        errorMessage:'username is required'
+    }
+}
+
+const passwordSchema={
+    notEmpty:{
+        errorMessage:'password is required'
     },
-    email:{
-        notEmpty:{
-            errorMessage:'email is required'
+    isLength:{
+        options:{
+            min:8,max:128
         },
-        isEmail:{
-            errorMessage:'invalid email format'
-        }
+        errorMessage:'password should be between 8 to 128 chars'
+    }
+}
+const emailRegisteredSchema={
+    notEmpty:{
+        errorMessage:'email is required'
     },
-    password:{
-        notEmpty:{
-            errorMessage:'password is required'
-        },
-        isLength:{
-            options:{
-                min:8,max:128
-            },
-            errorMessage:'password should be between 8 to 128 chars'
+    isEmail:{
+        errorMessage:'invalid email format'
+    },
+    custom:{
+        options:async(value)=>{
+            const user=await User.findOne({email:value})
+            if(user){
+                throw new Error('email already registered')
+            }else{
+                return true
+            }
         }
     }
 }
-module.exports=userValidationSchema
+const emailLoginSchema={
+    notEmpty:{
+        errorMessage:'email is required'
+    },
+    isEmail:{
+        errorMessage:'invalid email format'
+    },
+}
+const userRegistrationValidationSchema={
+    username:usernameSchema,
+    email:emailRegisteredSchema,
+    password:passwordSchema
+}
+const userLoginValidationSchema={
+    email:emailLoginSchema,
+    password:passwordSchema
+}
+module.exports={
+    userRegistrationValidationSchema,
+    userLoginValidationSchema
+}
--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const cors=require('cors')
 const configDB = require('./config/db')
 const {checkSchema}=require('express-validator')
 const usersController = require('./app/controllers/users-controller')
-const userValidationSchema = require('./app/helpers/user-validation')
+const { userRegistrationValidationSchema } = require('./app/helpers/user-validation')
+
 const app=express()
 const port=3018
 app.use(express.json())
 app.use(cors())
 configDB()
 
-app.post('/auth/register', checkSchema(userValidationSchema), usersController.register)
+app.post('/auth/register', checkSchema(userRegistrationValidationSchema), usersController.register)
 app.listen(port,()=>{
     console.log('server running on port',port)
 })

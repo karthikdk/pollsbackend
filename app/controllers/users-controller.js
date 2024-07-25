bf115c68f4106cd5e8c1aa7a9c9bb275e5a53674
KARTHIK D <dk.karthikd9@gmail.com>
2024-07-25 15:14:05 +0530

    login controller

--- app/controllers/users-controller.js
+++ app/controllers/users-controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const pick=require('lodash/pick')
+const jwt=require('jsonwebtoken')
 const bcryptjs=require('bcryptjs')
 const {validationResult}=require('express-validator')
 
@@ -24,6 +25,30 @@ usersController.register=async(req,res)=>{
         })
     } catch (error) {
         res.json(error)
+    }
+}
+
+usersController.login=async(req,res)=>{
+    const errors=validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    const body=pick(req.body,['email','password'])  
+    try {
+        const user=await User.findOne({email:body.email})
+        if(!user){
+            return res.status(404).json({errors:'invalid email/password'})
+        }
+        const result=await bcryptjs.compare(body.password,user.password)
+        if(!result){
+            return res.status(404).json({errors:'invalid email/password'})
+        }
+        const tokenData={id:user._id}
+        const token=jwt.sign(tokenData,process.env.JWT_SECRET,{expiresIn:'7d'})
+        res.json({token:`Bearer ${token}`})
+
+    } catch (error) {
+        res.status(500).json(error)
     }
 }
 
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors=require('cors')
 const configDB = require('./config/db')
 const {checkSchema}=require('express-validator')
 const usersController = require('./app/controllers/users-controller')
-const { userRegistrationValidationSchema } = require('./app/helpers/user-validation')
+const { userRegistrationValidationSchema, userLoginValidationSchema } = require('./app/helpers/user-validation')
 
 const app=express()
 const port=3018
@@ -13,6 +13,7 @@ app.use(cors())
 configDB()
 
 app.post('/auth/register', checkSchema(userRegistrationValidationSchema), usersController.register)
+app.post('/auth/login',checkSchema(userLoginValidationSchema),usersController.login)
 app.listen(port,()=>{
     console.log('server running on port',port)
 })

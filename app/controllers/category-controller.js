9ed00ceb228d6d97aa2169186b739ee3f9b52661
KARTHIK D <dk.karthikd9@gmail.com>
2024-08-02 18:28:22 +0530

    poll model

--- app/controllers/category-controller.js
+++ app/controllers/category-controller.js
@@ -6,8 +6,8 @@ categoryController.list=async(req,res)=>{
     try {
         const categories=await Category.find()
         res.json(categories)
-    } catch (error) {
-        res.status(500).json(error)
+    } catch (e) {
+        res.status(500).json(e)
     }
 }
 categoryController.create=async(req,res)=>{
@@ -18,20 +18,13 @@ categoryController.create=async(req,res)=>{
         })
     }
     const body=req.body
-    const categoryObj=await Category.findOne({name:{'$regex':body.name,$options:'i'}})
-    if(!categoryObj){
-        const category=new Category(body)
-        try {
-            await category.save()
-             res.json(category)
-        } catch (error) {
-            res.status(500).json(error)
-        } 
-    }else{
-        res.json({
-            error:'Category alreday present'
-        })
-    }
-}
+    const category=new Category(body)
+    try {
+        await category.save()
+        res.json(category)
+    } catch (e) {
+        res.status(500).json(e)
+    }         
+}   
         
 module.exports=categoryController
